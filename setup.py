605fbe1f735f73dcec47536d2e3bfbf5a7d1d39b
Shuo Li <shuoli84@gmail.com>
2014-05-06 20:06:09 +0800

    Create the dist 0.1

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="fabric-component",
+    name="fabric-components",
     author="lishuo",
     author_email="shuoli84@gmail.com",
+    url="https://github.com/shuoli84/fabric-components",
     version="0.1",
-    packages=['fabric-component'],
+    packages=['fabric-components'],
     license='MIT',
     long_description=open('README.txt').read()
 )
