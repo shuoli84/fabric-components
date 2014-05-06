94e9180892d48df2ce8183904e848f85f81a3f6f
Shuo Li <shuoli84@gmail.com>
2014-05-06 20:47:57 +0800

    - to _ to prevent load failure

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="fabric-components",
+    name="fabric_components",
     author="lishuo",
     author_email="shuoli84@gmail.com",
     url="https://github.com/shuoli84/fabric-components",
     version="0.1",
-    packages=['fabric-components'],
+    packages=['fabric_components'],
     license='MIT',
     long_description=open('README.txt').read()
 )
