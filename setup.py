1df0a75d8743b1a09da69fb6797fe8829354ab56
Shuo Li <shuoli84@gmail.com>
2014-05-07 16:50:02 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="lishuo",
     author_email="shuoli84@gmail.com",
     url="https://github.com/shuoli84/fabric-components",
-    version="0.1",
+    version="0.1.1",
     packages=['fabric_components'],
     license='MIT',
     long_description=open('README.txt').read()
